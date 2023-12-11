1574596171bb791fa1cb4cbb03af3312369dc533
HasseBillyBoy <125042713+HasseBillyBoy@users.noreply.github.com>
2023-12-11 14:13:51 +0100

    fetch og mapping af data med alans help + 4 webp

--- src/js/mainjs.js
+++ src/js/mainjs.js
@@ -1,5 +1,7 @@
+// Har fulgt nedenstående playliste ang. Supabase for at få hul igennem:
+// https://youtube.com/playlist?list=PLCx1FpZ4Dtb10T6cClBrq7jHky5HmVots&si=6IOaM4Va_fm1Pt3
+
 const url = "https://clijcnedvgtppqzmwicf.supabase.co";
-w;
 
 const key =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsaWpjbmVkdmd0cHBxem13aWNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA3Mzc1NDYsImV4cCI6MjAxNjMxMzU0Nn0.ZB94RLQFTwNWgedKWPsDaUC6axpOb001oC9_gVqzCnY";
@@ -14,5 +16,5 @@ fetch("https://clijcnedvgtppqzmwicf.supabase.co/rest/v1/artwork", {
   .then(showData);
 
 function showData(artwork) {
-  console.log(artwork);
+  console.table(artwork);
 }
